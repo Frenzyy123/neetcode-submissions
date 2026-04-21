19b23bf72643f192bc2caac6ac9103fa94fb5480
Frenzyy123 <jovan.pesic17@gmail.com>
2026-04-21 14:39:02 +0200

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        letters_s = {}
+        for char in s:
+            if char not in letters_s:
+                letters_s[char] = 1
+            else:
+                letters_s[char] += 1
+        for char in t:
+            if char not in letters_s:
+                return False
+            elif letters_s[char] == 0:
+                return False
+            else:
+                letters_s[char] -= 1
+        for i in letters_s:
+            if letters_s[i] != 0:
+                return False
+        return True
