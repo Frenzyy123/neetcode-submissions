5cd672483a6eb9420cd0f8796359a92d45c2f20d
Frenzyy123 <jovan.pesic17@gmail.com>
2026-06-25 11:48:55 +0200

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, nums , target: int) :
+        nums.sort()
+        output = []
+        comb = []
+        def backtrack(index):
+            if sum(comb) == target:
+                output.append(comb.copy())
+                return
+            elif sum(comb) > target:
+                return
+
+            for i in range(index,len(nums)):
+                if nums[i] + sum(comb) <= target:
+                    comb.append(nums[i])
+                    backtrack(i)
+                    comb.pop()
+                else:
+                    return
+        backtrack(0)
+        return output
